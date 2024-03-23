3e43c244da98a87637cdec7d6a75a5077c738d72
M. K. Gaikwad <yavan@outlook.com>
2024-03-23 12:58:21 +0530

    Create clean_jokes.py

---
+++ clean_jokes.py
@@ -0,0 +1,365 @@
+#https://parade.com/1041830/marynliles/clean-jokes/
+SKIP TO MAIN CONTENT
+
+Parade home
+TRENDING NEWS
+ENTERTAINMENT
+FOOD & DRINK
+PETS
+SHOPPING
+HEALTHY NOW
+ASTROLOGY
+NEWSLETTERS
+HOME
+ENTERTAINMENT
+101 Good, Clean Jokes That'll Make You Laugh Your Pants Off
+Share these funny jokes that adults will appreciate.
+MARYN LILESUPDATED:FEB 23, 2024
+Clean jokes
+iStock
+
+Funny clean jokes make every conversation better—whether you're sharing a laugh with a friend or entertaining your kids—and these G-rated jokes are no exception.
+
+From the best clean jokes for adults to clean jokes of the day, this big list has something for everyone, so you can feel good about busting out these hilarious SFW funnies, no matter who it is you're talking to—from your grandma to your coworker.
+
+Read on to discover the best clean jokes that promise a whole lot of giggles for both adults and kids alike.
+
+
+exp-player-logo
+
+Read More
+
+101 Clean Jokes
+1. Did you hear about the first restaurant to open on the moon?It had great food, but no atmosphere.
+
+2. Why don’t scientists trust atoms?
+Because they make up everything!
+
+3. What do dentists call their x-rays?
+Tooth pics!
+
+4. What did one ocean say to the other ocean?
+Nothing, it just waved.
+
+5. Do you want to hear a construction joke?
+Sorry, I’m still working on it.
+
+Clean joke that's funny for adults.
+Unsplash
+
+6. Did you hear about the fire at the circus?
+It was in tents!
+
+7. Why do ducks have feathers?
+To cover their butt quacks!
+
+8. What’s the difference between a hippo and a zippo?
+One is really heavy and the other’s a little lighter.
+
+9. What does a nosey pepper do?
+It gets jalapeño business.
+
+10. Why should you never trust stairs?
+They’re always up to something.
+
+11. When does a joke become a ‘dad’ joke?
+When it becomes apparent.
+
+Related: Christian Jokes
+
+12. Why did the bullet end up losing his job?
+He got fired.
+
+13. What kind of shorts do clouds wear?
+Thunderpants
+
+14. I entered ten puns in a contest to see which would win.
+No pun in ten did.
+
+15. How do you measure a snake?
+In inches—they don’t have feet.
+
+16. Where does a waitress with only one leg work?
+IHOP.
+
+17. What does a house wear?
+Address!
+
+18. Why are toilets always so good at poker?
+They always get a flush
+
+19. Why is Peter Pan always flying?
+Because he Neverlands. (I love this joke because it never grows old.)
+
+20. You heard the rumor going around about butter?
+Never mind, I shouldn’t spread it.
+
+21. Two windmills are standing on a wind farm. One asks, ‘What’s your favorite kind of music?’
+The other replies, ‘I’m a big metal fan.’
+
+22. The first rule of the Alzheimer’s club is…
+Wait, where are we again?
+
+23. I took the shell off of my racing snail, thinking it would make him faster.
+But if anything, it made him more sluggish.
+
+24. What do you get from a pampered cow?
+Spoiled milk.
+
+25. How does NASA organize a party?
+They planet.
+
+Unsplash
+
+26. What’s the best thing about Switzerland?
+I don’t know, but the flag is a big plus.
+
+27. Why did the bicycle fall over?
+Because it was two-tired!
+
+28. How many tickles does it take to get an octopus to laugh?
+Ten tickles
+
+29. Why doesn’t Dracula have any friends?
+Well, honestly, he’s a real pain in the neck.
+
+30. My teachers told me I’d never amount to much since I procrastinate so much.
+I told them, “Just you wait!”
+
+31. Why were they called the “dark ages?”
+Because there were a lot of knights.
+
+32. What gets wetter the more it dries?
+A towel.
+
+33. Why aren’t koalas considered bears?
+They don’t have the right koala-fications.
+
+34. You: What cartoon mouse walks on two feet?
+
+Them: Mickey Mouse
+
+You: What duck walks on two feet?
+
+Them: Donald Duck
+
+You: No, all ducks do!
+
+35. Want to hear a joke about a roof?
+The first one’s on the house.
+
+36. What’s a pirate’s favorite letter?
+You probably think it’s “R” but it be the “C”.
+
+37. How much teddy bears never want to eat anything?
+Because they’re always stuffed.
+
+38. Did you hear about the cheese factory that exploded in France?
+There was nothing left but de Brie.
+
+39. Where should you go in the room if you’re feeling cold?
+The corner—they’re usually 90 degrees.
+
+40. I can never take my dog to the park because the ducks keep trying to bite him.
+I guess that’s what I get for buying a pure bread dog.
+
+41. What’s the difference between a poorly dressed man on a unicycle and a well-dressed man on a bicycle?
+Attire.
+
+42. What did the Buddhist ask the hot dog vendor?
+“Make me one with everything.”
+
+43. You know why you never see elephants hiding up in trees?
+Because they’re really good at it.
+
+44. A horse walks into a bar.
+The bartender says, “Why the long face?”
+
+45. How did the hipster burn his mouth?
+He ate his pizza before it was cool.
+
+Unsplash
+
+46. What do you get when you cross a dyslexic, an insomniac, and an agnostic?
+Someone who lays awake at night wondering if there’s a dog.
+
+47. As a scarecrow, people say I’m outstanding in my field.
+But hay, it’s in my jeans.
+
+48. Why don’t blind people skydive?
+Because it scares their dogs.
+
+49. What’s the difference between an oral thermometer and a rectal thermometer?
+The taste, mostly.
+
+50. I stayed up all night and tried to figure out where the sun was.
+Then it dawned on me.
+
+Related: Halloween Jokes
+
+51. I told my friend ten jokes to make him laugh.
+Sadly, no pun in ten did.
+
+52. I couldn’t believe the highway department called my dad a thief.
+But when I got home, the signs were all there.
+
+53. Why did the cowboy get a wiener dog?
+He wanted to get a long little doggie.
+
+54. Can a kangaroo jump higher than the Empire State Building?
+Of course! The Empire State Building can’t jump.
+
+55. What did the duck say when it bought some lipstick?
+“Put it on my bill.”
+
+56. George Clooney, Leonardo DiCaprio, and Matthew McConaughey get together to make a movie.
+
+Clooney says, “I’ll direct.”
+
+DiCaprio says, “I’ll act.”
+
+McConaughey says, “I’ll write, I’ll write, I’ll write.”
+
+57. A horse walks into a bar.
+
+The bartender says, “Hey!”
+
+The horse replies, “Sure.”
+
+58. I googled “Rorshach test.”
+But for some reason, all that came up were pictures of my parents fighting.
+
+59. What do we want?
+
+Low-flying airplane noises!
+
+When do we want them?
+
+Nnnnneeeeeeeeeeoooooooooow!
+
+60. I tried to win a suntanning competition.
+But all I got was bronze.
+
+61. What do you call fake spaghetti?
+An impasta!
+
+62. Why don’t seagulls fly over the bay?
+Because then they’d be bagels.
+
+63. What do you call malware on a Kindle?
+A bookworm.
+
+64. Did you hear about the painter who was hospitalized?
+The doctors say it was due to too many strokes.
+
+65. Why are crabs so bad at sharing?
+Because they’re all shellfish.
+
+Unsplash
+
+66. What did the tie say to the hat?
+You go on ahead. I’ll hang around.
+
+67. How do you make a tissue dance?
+Put a little boogie in it.
+
+68. What do you call a cow with a twitch?
+Beef jerky.
+
+69. I started a new job as a tailor last week.
+It’s been sew-sew.
+
+70. What concert only costs 45 cents?
+50 Cent featuring Nickleback.
+
+71. What kind of shoes does a spy wear?
+Sneakers.
+
+72. I’ve been trying to make a sarcastic club, but it’s been really hard to tell if people are interested in joining or not.
+
+73. And the Lord said unto John, “Come forth and you will receive eternal life.”
+But John came fifth and won a toaster.
+
+74. Why don’t some fish play piano?
+Because you can’t tuna fish!
+
+75. My wife accused me the other day of being too immature.
+I told her there were no girls allowed in my fort.
+
+Clean Jokes for Adults
+76. Someone stole my mood ring yesterday.
+I still don’t know how I feel about that.
+
+77. A Roman legionnaire walks into a bar, holds up two fingers and says, “Five beers, please.”
+
+78. I tried to catch fog yesterday.
+Mist.
+
+79. Working in a mirror factory is something I can totally see myself doing.
+
+80. Someone stole my Microsoft Office and they’re gonna pay.
+You have my Word.
+
+81. What do you call a dog with no legs?
+It doesn’t matter, it’s not going to come anyway.
+
+82. You know what they say about cliffhangers…
+
+83. Why does a chicken coop only have two doors?
+Because if it had four doors, it would be a chicken sedan.
+
+84. A sandwich walks into a bar.
+The bartender says, “Sorry, we don’t serve food here.”
+
+85. I got a new job last week as the new top dog at Old MacDonald’s farm.
+I’m the new C-I-E-I-O.
+
+Unsplash
+
+86. The sky was looking ominous so I asked Siri, “Surely, it’s not going to rain today?”
+
+And she replied, “Yes it is, and don’t call me Shirley.”
+
+That was when I realized I’d left my phone on Airplane mode.
+
+87. Why did the giraffe get such bad grades?
+He always had his head stuck in the clouds.
+
+88. Why are frogs always so happy?
+They eat whatever bugs them.
+
+89. Whenever you jump on a trampoline, did you know it changes the season?
+No matter what time of year, it always becomes spring time.
+
+90. Why didn’t the skeleton go to the dance?
+Because he had no body to go with.
+
+91. What do you call a musician with problems?
+A trebled man.
+
+92. Where do snowmen keep their savings?
+In the snowbank.
+
+93. What do you call a religious person who sleepwalks?
+A roamin’ Catholic.
+
+94. Did you hear about the carrot detective?
+He always got to the root of every case.
+
+95. What washes up on very small beaches?
+Micro-waves
+
+96. What did one elevator say to the other?
+I think I’m coming down with something.
+
+97. What happened when a faucet, a tomato, and some lettuce ran a race together?
+The lettuce was ahead, the faucet was running, and the tomato was trying to ketchup.
+
+"What do you call a belt with a watch on it?
+A waist of time.",
+"Why did the tomato turn red?
+It saw the salad dressing.",
+"What did the grape do when it got stepped on?
+It let out a little wine.",
+"Why won’t skeletons fight each other?
+They just don’t have the guts."]
