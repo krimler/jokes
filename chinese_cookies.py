3031c6e4e40a26d425184c515f3bebe8e906675b
M. K. Gaikwad <yavan@outlook.com>
2024-03-23 19:02:20 +0530

    Create chinese_cookies.py

---
+++ chinese_cookies.py
@@ -0,0 +1,22 @@
+cpy = [
+'If failure really was the best teacher, users would all be geniuses by now.',
+'The pursuit of IT uptime can negatively affect enjoying personal downtime.',
+'Remember … Don’t copy that floppy!',
+'The early patch blocks the worm.',
+'Microsoft Word crashing is the new “dog ate my homework.”,,
+'Said the movie cowboy to the stubborn macOS process: “I can’t force-quit you.”,,
+'Spiciness is they key to happiness.',
+'Counting on the NSA to save your data is not a solid backup strategy.',
+'Be glad you didn’t get a FORTRAN cookie. It would have been way past it’s expiration date.',
+'Pro tip for users: There is no “any key.”',
+'To err is human, to err all too often is user.',
+'An IT pro’s work is never done.',
+'You look like you could use a beer right now.',
+'When making New Year’s resolutions, avoid 800 x 600.',
+'A closed end user’s mouth gathers no feet.',
+'You will soon hear from a long lost %USERNAME%',
+'Passwords are like underwear. Change them regularly.',
+'May your systems never hang, nor go out with a bang.',
+'Time is an illusion. Lunchtime doubly so (especially for IT pros) — via Instinct via The Hitchhiker’s Guide to the Galaxy.',
+  
+]
