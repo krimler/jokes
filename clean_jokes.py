8e19e78691352279190da8b774cf2a888a75077b
M. K. Gaikwad <yavan@outlook.com>
2024-03-24 11:23:10 +0530

    Update clean_jokes.py

--- clean_jokes.py
+++ clean_jokes.py
@@ -32,190 +32,77 @@ jokes = ['Did you hear about the first restaurant to open on the moon?It had gre
 'My teachers told me I’d never amount to much since I procrastinate so much. I told them, “Just you wait!”',
 'Why were they called the “dark ages?” Because there were a lot of knights.',
 'What gets wetter the more it dries? A towel.',
-'Why aren’t koalas considered bears?',
-'They don’t have the right koala-fications.',
+'Why aren’t koalas considered bears? They don’t have the right koala-fications.',
 'You: What cartoon mouse walks on two feet? Them: Mickey Mouse',
 'You: What duck walks on two feet? Them: Donald Duck You: No, all ducks do!',
 'Want to hear a joke about a roof? The first one’s on the house.',
-'What’s a pirate’s favorite letter? You probably think it’s “R” but it be the “C”.
-'How much teddy bears never want to eat anything? Because they’re always stuffed.
-'Did you hear about the cheese factory that exploded in France? There was nothing left but de Brie.
-'Where should you go in the room if you’re feeling cold? The corner—they’re usually 90 degrees.
-'I can never take my dog to the park because the ducks keep trying to bite him. I guess that’s what I get for buying a pure bread dog.
-'What’s the difference between a poorly dressed man on a unicycle and a well-dressed man on a bicycle? Attire.
-'What did the Buddhist ask the hot dog vendor? “Make me one with everything.”
-'You know why you never see elephants hiding up in trees? Because they’re really good at it.
-'A horse walks into a bar. The bartender says, “Why the long face?”
-'How did the hipster burn his mouth? He ate his pizza before it was cool.
-'What do you get when you cross a dyslexic, an insomniac, and an agnostic? Someone who lays awake at night wondering if there’s a dog.
-'As a scarecrow, people say I’m outstanding in my field. But hay, it’s in my jeans.
-'Why don’t blind people skydive? Because it scares their dogs.
-'What’s the difference between an oral thermometer and a rectal thermometer? The taste, mostly.
-'I stayed up all night and tried to figure out where the sun was. Then it dawned on me.
+'What’s a pirate’s favorite letter? You probably think it’s “R” but it be the “C”.',
+'How much teddy bears never want to eat anything? Because they’re always stuffed.',
+'Did you hear about the cheese factory that exploded in France? There was nothing left but de Brie.',
+'Where should you go in the room if you’re feeling cold? The corner—they’re usually 90 degrees.',
+'I can never take my dog to the park because the ducks keep trying to bite him. I guess that’s what I get for buying a pure bread dog.',
+'What’s the difference between a poorly dressed man on a unicycle and a well-dressed man on a bicycle? Attire.',
+'What did the Buddhist ask the hot dog vendor? “Make me one with everything.”',
+'You know why you never see elephants hiding up in trees? Because they’re really good at it.',
+'A horse walks into a bar. The bartender says, “Why the long face?”',
+'How did the hipster burn his mouth? He ate his pizza before it was cool.',
+'What do you get when you cross a dyslexic, an insomniac, and an agnostic? Someone who lays awake at night wondering if there’s a dog.',
+'As a scarecrow, people say I’m outstanding in my field. But hay, it’s in my jeans.',
+'Why don’t blind people skydive? Because it scares their dogs.',
+'What’s the difference between an oral thermometer and a rectal thermometer? The taste, mostly.',
+'I stayed up all night and tried to figure out where the sun was. Then it dawned on me.',
+'I told my friend ten jokes to make him laugh. Sadly, no pun in ten did.',
+'I couldn’t believe the highway department called my dad a thief. But when I got home, the signs were all there.',
+'Why did the cowboy get a wiener dog? He wanted to get a long little doggie.',
+'Can a kangaroo jump higher than the Empire State Building? Of course! The Empire State Building can’t jump.',
+'What did the duck say when it bought some lipstick? “Put it on my bill.”',
 
-51. I told my friend ten jokes to make him laugh.
-Sadly, no pun in ten did.
-
-52. I couldn’t believe the highway department called my dad a thief.
-But when I got home, the signs were all there.
-
-53. Why did the cowboy get a wiener dog?
-He wanted to get a long little doggie.
-
-54. Can a kangaroo jump higher than the Empire State Building?
-Of course! The Empire State Building can’t jump.
-
-55. What did the duck say when it bought some lipstick?
-“Put it on my bill.”
-
-56. George Clooney, Leonardo DiCaprio, and Matthew McConaughey get together to make a movie.
-
-Clooney says, “I’ll direct.”
-
-DiCaprio says, “I’ll act.”
-
-McConaughey says, “I’ll write, I’ll write, I’ll write.”
-
-57. A horse walks into a bar.
-
-The bartender says, “Hey!”
-
-The horse replies, “Sure.”
-
-58. I googled “Rorshach test.”
-But for some reason, all that came up were pictures of my parents fighting.
-
-59. What do we want?
-
-Low-flying airplane noises!
-
-When do we want them?
-
-Nnnnneeeeeeeeeeoooooooooow!
-
-60. I tried to win a suntanning competition.
-But all I got was bronze.
-
-61. What do you call fake spaghetti?
-An impasta!
-
-62. Why don’t seagulls fly over the bay?
-Because then they’d be bagels.
-
-63. What do you call malware on a Kindle?
-A bookworm.
-
-64. Did you hear about the painter who was hospitalized?
-The doctors say it was due to too many strokes.
-
-65. Why are crabs so bad at sharing?
-Because they’re all shellfish.
-
-Unsplash
-
-66. What did the tie say to the hat?
-You go on ahead. I’ll hang around.
-
-67. How do you make a tissue dance?
-Put a little boogie in it.
-
-68. What do you call a cow with a twitch?
-Beef jerky.
-
-69. I started a new job as a tailor last week.
-It’s been sew-sew.
-
-70. What concert only costs 45 cents?
-50 Cent featuring Nickleback.
-
-71. What kind of shoes does a spy wear?
-Sneakers.
-
-72. I’ve been trying to make a sarcastic club, but it’s been really hard to tell if people are interested in joining or not.
-
-73. And the Lord said unto John, “Come forth and you will receive eternal life.”
-But John came fifth and won a toaster.
-
-74. Why don’t some fish play piano?
-Because you can’t tuna fish!
-
-75. My wife accused me the other day of being too immature.
-I told her there were no girls allowed in my fort.
-
-Clean Jokes for Adults
-76. Someone stole my mood ring yesterday.
-I still don’t know how I feel about that.
-
-77. A Roman legionnaire walks into a bar, holds up two fingers and says, “Five beers, please.”
-
-78. I tried to catch fog yesterday.
-Mist.
-
-79. Working in a mirror factory is something I can totally see myself doing.
-
-80. Someone stole my Microsoft Office and they’re gonna pay.
-You have my Word.
-
-81. What do you call a dog with no legs?
-It doesn’t matter, it’s not going to come anyway.
-
-82. You know what they say about cliffhangers…
-
-83. Why does a chicken coop only have two doors?
-Because if it had four doors, it would be a chicken sedan.
-
-84. A sandwich walks into a bar.
-The bartender says, “Sorry, we don’t serve food here.”
-
-85. I got a new job last week as the new top dog at Old MacDonald’s farm.
-I’m the new C-I-E-I-O.
-
-Unsplash
-
-86. The sky was looking ominous so I asked Siri, “Surely, it’s not going to rain today?”
-
-And she replied, “Yes it is, and don’t call me Shirley.”
-
-That was when I realized I’d left my phone on Airplane mode.
-
-87. Why did the giraffe get such bad grades?
-He always had his head stuck in the clouds.
-
-88. Why are frogs always so happy?
-They eat whatever bugs them.
-
-89. Whenever you jump on a trampoline, did you know it changes the season?
-No matter what time of year, it always becomes spring time.
-
-90. Why didn’t the skeleton go to the dance?
-Because he had no body to go with.
-
-91. What do you call a musician with problems?
-A trebled man.
-
-92. Where do snowmen keep their savings?
-In the snowbank.
-
-93. What do you call a religious person who sleepwalks?
-A roamin’ Catholic.
-
-94. Did you hear about the carrot detective?
-He always got to the root of every case.
-
-95. What washes up on very small beaches?
-Micro-waves
-
-96. What did one elevator say to the other?
-I think I’m coming down with something.
-
-97. What happened when a faucet, a tomato, and some lettuce ran a race together?
-The lettuce was ahead, the faucet was running, and the tomato was trying to ketchup.
-
-"What do you call a belt with a watch on it?
-A waist of time.",
-"Why did the tomato turn red?
-It saw the salad dressing.",
-"What did the grape do when it got stepped on?
-It let out a little wine.",
-"Why won’t skeletons fight each other?
-They just don’t have the guts."]
+'George Clooney, Leonardo DiCaprio, and Matthew McConaughey get together to make a movie. Clooney says, “I’ll direct.”',
+DiCaprio says, “I’ll act.” McConaughey says, “I’ll write, I’ll write, I’ll write.”',
+'A horse walks into a bar. The bartender says, “Hey!” The horse replies, “Sure.”',
+'I googled “Rorshach test.” But for some reason, all that came up were pictures of my parents fighting.',
+'What do we want? Low-flying airplane noises! When do we want them? Nnnnneeeeeeeeeeoooooooooow!',
+'I tried to win a suntanning competition. But all I got was bronze.',
+'What do you call fake spaghetti? An impasta!',
+'Why don’t seagulls fly over the bay? Because then they’d be bagels.',
+',What do you call malware on a Kindle? A bookworm.',
+'Did you hear about the painter who was hospitalized? The doctors say it was due to too many strokes.',
+'Why are crabs so bad at sharing? Because they’re all shellfish.',
+'What did the tie say to the hat? You go on ahead. I’ll hang around.',
+'How do you make a tissue dance? Put a little boogie in it.',
+'What do you call a cow with a twitch? Beef jerky.',
+'I started a new job as a tailor last week. It’s been sew-sew.',
+'What concert only costs 45 cents? 50 Cent featuring Nickleback.',
+'What kind of shoes does a spy wear? Sneakers.',
+'I’ve been trying to make a sarcastic club, but it’s been really hard to tell if people are interested in joining or not.',
+'And the Lord said unto John, “Come forth and you will receive eternal life.” But John came fifth and won a toaster.',
+'Why don’t some fish play piano? Because you can’t tuna fish!',
+'My wife accused me the other day of being too immature. I told her there were no girls allowed in my fort.',',
+'Someone stole my mood ring yesterday. I still don’t know how I feel about that.',
+'A Roman legionnaire walks into a bar, holds up two fingers and says, “Five beers, please.”',
+'I tried to catch fog yesterday. Mist.',
+'Working in a mirror factory is something I can totally see myself doing.',
+'Someone stole my Microsoft Office and they’re gonna pay. You have my Word.',
+'What do you call a dog with no legs? It doesn’t matter, it’s not going to come anyway.',
+'You know what they say about cliffhangers…
+'Why does a chicken coop only have two doors? Because if it had four doors, it would be a chicken sedan.',
+'A sandwich walks into a bar. The bartender says, “Sorry, we don’t serve food here.”',
+'I got a new job last week as the new top dog at Old MacDonald’s farm. I’m the new C-I-E-I-O.',
+'The sky was looking ominous so I asked Siri, “Surely, it’s not going to rain today?”\
+And she replied, “Yes it is, and don’t call me Shirley.”\
+That was when I realized I’d left my phone on Airplane mode.',
+'Why did the giraffe get such bad grades? He always had his head stuck in the clouds.',
+'Why are frogs always so happy? They eat whatever bugs them.',
+'Whenever you jump on a trampoline, did you know it changes the season? No matter what time of year, it always becomes spring time.',
+'Why didn’t the skeleton go to the dance? Because he had no body to go with.',
+'What do you call a musician with problems? A trebled man.',
+'Where do snowmen keep their savings? In the snowbank.',
+'What do you call a religious person who sleepwalks? A roamin’ Catholic.',
+'Did you hear about the carrot detective? He always got to the root of every case.',
+'What washes up on very small beaches? Micro-waves.',
+'What did one elevator say to the other? I think I’m coming down with something.',
+'What happened when a faucet, a tomato, and some lettuce ran a race together? The lettuce was ahead, the faucet was running, and the tomato was trying to ketchup.',
+'What do you call a belt with a watch on it? A waist of time.',
+'Why did the tomato turn red? It saw the salad dressing.',
+'What did the grape do when it got stepped on? It let out a little wine.',
+'Why won’t skeletons fight each other? They just don’t have the guts.']
